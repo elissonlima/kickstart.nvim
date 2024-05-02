fcff52e71d72df83af5bc01235667c5bef473fe6
Elisson Lima <31016448+elissonlima@users.noreply.github.com>
2024-05-02 19:41:53 -0300

    Updates on rust-analyzer

--- init.lua
+++ init.lua
@@ -568,7 +568,22 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {
+          imports = {
+            granularity = {
+              group = 'module',
+            },
+            prefix = 'self',
+          },
+          cargo = {
+            buildScripts = {
+              enable = true,
+            },
+          },
+          procMacro = {
+            enable = true,
+          },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
